fa2268aac37d7385a301cbb1665ca5f2c6e8525c
rumiflow1 <rumiflow1@gmail.com>
2026-09-04 11:42:13 +0500

    Activate global production API compatibility bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './services/api';
+import './services/axiosBridge';
 import App from './App.tsx';
 import './index.css';
 
